4b2e2ac97b6a8a1890bce17bec36d7edfc5a1f27
LuuVanDat-3012 <lovanmet0512@gmail.com>
2021-04-27 10:21:21 +0700

    viế body-right

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
   <div id="app">
-  
+     <BodyLeft></BodyLeft>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BodyLeft from "./components/Body-left.vue"
 export default {
   name: 'App',
   components: {
-   
+   BodyLeft
+  },
+  data(){
+    return{
+      name : "luu van dat"
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Body-left.vue
@@ -0,0 +1,135 @@
+<template>
+   <div class="body-left">
+       <div class="sidebar">
+           <div class="toggle"></div>
+           <div class="logo"></div>
+       </div>
+
+       <div class="menu">
+            <div class="menu-item">
+                <div class="icon overview"></div>
+                <div>Tổng quan</div>
+            </div>
+             <div class="menu-item">
+                <div class="icon report"></div>
+                <div>Báo cáo</div>
+            </div>
+             <div class="menu-item">
+                <div class="icon buy"></div>
+                <div>Mua hàng</div>
+            </div>
+       </div>
+      
+   </div>
+    
+</template>
+<script>
+export default {
+   name : "body-left",
+   props:{
+       name :{
+           type: String,
+           default : null
+       }
+   }
+
+   
+}
+</script>
+
+<style scope>
+   .body-left {
+    background-color: #ffffff;
+    width: 200px;
+    height: 100%;
+    border-right: 1px solid #bbbbbb;
+    display: flex;
+    flex-direction: column;
+}
+
+    .body-left .sidebar {
+        height: 50px;
+        display: flex;
+    }
+
+        .body-left .sidebar .toggle {
+            width: 50px;
+            height: 50px;
+            background-image: url('../content/image/toggle.png');
+            background-repeat: no-repeat;
+            background-size: 22px;
+            background-position: 18px center;
+            cursor: pointer;
+        }
+
+        .body-left .sidebar .logo {
+            width: 138px;
+            height: 50px;
+            background-image: url('../content/image/cukcuk-logo.png');
+            background-size: 126px;
+            background-repeat: no-repeat;
+            background-position: 0px 9px;
+            cursor: pointer;
+        }
+
+    .body-left .menu {
+        position: relative;
+        flex: 1;
+    }
+
+        .body-left .menu .menu-item {
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+        }
+
+            .body-left .menu .menu-item:hover {
+                background-color: #E9EBEE;
+            }
+
+            .body-left .menu .menu-item .icon {
+                width: 50px;
+                height: 45px;
+                background-repeat: no-repeat;
+                background-size: 22px;
+                background-position: 18px center;
+            }
+
+                .body-left .menu .menu-item .icon.overview {
+                    background-image: url('../content/icon/dashboard.png');
+                }
+
+                .body-left .menu .menu-item .icon.report {
+                    background-image: url('../content/icon/report.png');
+                }
+
+            .body-left .menu .menu-item .item-name {
+                width: 150px;
+            }
+
+.body-right {
+    position: absolute;
+    left: 201px;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color: bisque;
+}
+
+    .body-right .header {
+        background-color: antiquewhite;
+        width: 100%;
+        height: 50px;
+        border-bottom: 1px solid #bbbbbb;
+    }
+
+    .body-right .content {
+        background-color: #E9EBEE;
+        position: absolute;
+        top: 51px;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+
+</style>
